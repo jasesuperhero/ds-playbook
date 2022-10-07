e528d5acc1fb89e3508cc2242944a57fe72d0c6a
Daniel Kalintsev <d.kalintsev@gmail.com>
2022-10-07 14:30:11 +0200

    Initial Commit

---
+++ DSPlaybook/DSPlaybookApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSPlaybookApp.swift
+//  DSPlaybook
+//
+//  Created by Daniel Kalintsev on 07.10.22.
+//
+
+import SwiftUI
+
+@main
+struct DSPlaybookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
